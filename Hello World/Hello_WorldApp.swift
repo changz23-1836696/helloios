8e6b795faeee3e134b7706094bbb2ce25ff7f6d8
changz23-1836696 <43832007+changz23-1836696@users.noreply.github.com>
2021-04-05 10:38:58 -0700

    Initial Commit

---
+++ Hello World/Hello_WorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_WorldApp.swift
+//  Hello World
+//
+//  Created by 曾畅 on 2021/4/5.
+//
+
+import SwiftUI
+
+@main
+struct Hello_WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
