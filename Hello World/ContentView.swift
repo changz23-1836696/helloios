8a0e4742a0c2645e4a01c562922fa26d811a8369
changz23-1836696 <43832007+changz23-1836696@users.noreply.github.com>
2021-04-05 21:23:43 -0700

    submit

--- Hello World/ContentView.swift
+++ Hello World/ContentView.swift
@@ -2,20 +2,22 @@
 //  ContentView.swift
 //  Hello World
 //
-//  Created by 曾畅 on 2021/4/5.
+//  Created by Chang on 2021/4/5.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Be Boundless!")
             .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
